eeb50b59f1046026c34450e38a88de896911bc9b
Eddie Villalba <eddie.villalba@microsoft.com>
2019-06-17 14:42:18 -0500

    Initial  build of secure aks build

---
+++ aks_advanced_config/main_aks.tf
@@ -0,0 +1,67 @@
+# service principal for aks
+
+resource "azurerm_kubernetes_cluster" "main" {
+  name                = "${var.PROJECT}${var.INSTANCE}${var.ENVIRONMENT}${random_integer.uuid.result}aks"
+  location            = "${azurerm_resource_group.main.location}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+  dns_prefix          = "${var.PROJECT}${var.INSTANCE}${var.ENVIRONMENT}${random_integer.uuid.result}"
+  kubernetes_version  = "${var.K8S_VER}"
+
+  depends_on = [
+   "azurerm_role_assignment.aks-network-contributor",
+   "azurerm_subnet.akssubnet"]
+
+  linux_profile {
+    admin_username = "${var.ADMIN_USER}"
+
+    ssh_key {
+      key_data = "${var.AKS_SSH_ADMIN_KEY}"
+    }
+  }
+
+  agent_pool_profile {
+    name            = "default"
+    count           = "${var.NODE_COUNT}"
+    vm_size         = "${var.NODE_SIZE}"
+    os_type         = "Linux"
+    os_disk_size_gb = 30
+    vnet_subnet_id  = "${azurerm_subnet.akssubnet.id}"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+    service_cidr = "${var.SERVICE_CIDR}"
+    dns_service_ip = "${var.DNS_IP}"
+    docker_bridge_cidr = "${var.DOCKER_CIDR}"
+  }
+
+  role_based_access_control {
+    enabled = true
+  }
+
+  service_principal {
+    client_id     = "${azuread_application.akssp.application_id}"
+    client_secret = "${azuread_service_principal_password.akssp.value}"
+  }
+
+  api_server_authorized_ip_ranges = [
+    "100.200.0.0/14",
+    "72.183.132.114/32",
+    "${azurerm_public_ip.azfwpip.ip_address}/32"
+  ]
+}
+# kube config and helm init
+resource "local_file" "kube_config" {
+  # kube config
+  filename = "${var.K8S_KUBE_CONFIG}"
+  content  = "${azurerm_kubernetes_cluster.main.kube_config_raw}"
+
+  # helm init
+  provisioner "local-exec" {
+    command = "helm init --client-only"
+    environment {
+      KUBECONFIG = "${var.K8S_KUBE_CONFIG}"
+      HELM_HOME  = "${var.K8S_HELM_HOME}"
+    }
+  }
+}
--- aks_advnet_rbac/variables.tf
+++ aks_advnet_rbac/variables.tf
@@ -75,3 +75,4 @@ variable "dockerbridge-cidr" {
 }
 
 
+
