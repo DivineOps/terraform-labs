da48677d72efeeb7e864f04b30083cd320be67dc
Eddie Villalba <eddie.villalba@microsoft.com>
2019-06-17 15:18:24 -0500

    removed kubeconfig output

--- aks_advanced_config/output.tf
+++ aks_advanced_config/output.tf
@@ -6,11 +6,6 @@ output "AKS_RESOURCE_GROUP" {
   value = "${azurerm_resource_group.main.name}"
   description = "The main resource group of the AKS Resource"
 }
-
-output "KUBE_CONFIG_PATH" {
-  value = "${local_file.kube_config.filename}"
-}
-
 output "AKS_SP_APPID" {
   value = "${azuread_application.akssp.application_id}"
 }
