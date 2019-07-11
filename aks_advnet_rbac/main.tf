6298b1c7291db5834f456577f2601a84e816e01b
DivineOps <sasrose@microsoft.com>
2019-07-11 08:42:47 -0500

    add location policy

--- aks_advnet_rbac/main.tf
+++ aks_advnet_rbac/main.tf
@@ -23,6 +23,23 @@ resource "azurerm_resource_group" "test" {
   name     = "${var.prefix}-aks-rg"
   location = "${var.location}"
 }
+
+resource "azurerm_policy_assignment" "test" {
+  name                 = "location-policy-assignment"
+  scope                = "${azurerm_resource_group.test.id}"
+  policy_definition_id = "${azurerm_policy_definition.test.id}"
+  description          = "Policy Assignment created via an Acceptance Test"
+  display_name         = "Location Policy Assignment"
+
+  parameters = <<PARAMETERS
+{
+  "allowedLocations": {
+    "value": [ ${var.location} ]
+  }
+}
+PARAMETERS
+}
+
 #Uncomment below if you need a Route Table (UDR) to route to an Netwokr Virtual Appliamce (Palo Alto, F5, Barricuda, Cisco ASR, etc) in a peered VNET
 # resource "azurerm_route_table" "test" {
 #   name                = "${var.prefix}-routetable"
