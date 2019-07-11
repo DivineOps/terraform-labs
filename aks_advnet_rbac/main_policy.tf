ce5df668cb774c68062873bb813c8bb97356a869
DivineOps <sasrose@microsoft.com>
2019-07-11 12:38:17 -0500

    test if commenting out policy definition works

--- aks_advnet_rbac/main_policy.tf
+++ aks_advnet_rbac/main_policy.tf
@@ -1,37 +1,37 @@
-# Define the policy to limit the deployment to a specific location
+# # Define the policy to limit the deployment to a specific location
 
-resource "azurerm_policy_definition" "test" {
-  name         = "location-policy-definition"
-  policy_type  = "Custom"
-  mode         = "All"
-  display_name = "location-policy-definition"
+# resource "azurerm_policy_definition" "test" {
+#   name         = "location-policy-definition"
+#   policy_type  = "Custom"
+#   mode         = "All"
+#   display_name = "location-policy-definition"
 
-  policy_rule = <<POLICY_RULE
-    {
-    "if": {
-      "not": {
-        "field": "location",
-        "in": "[parameters('allowedLocations')]"
-      }
-    },
-    "then": {
-      "effect": "audit"
-    }
-  }
-POLICY_RULE
+#   policy_rule = <<POLICY_RULE
+#     {
+#     "if": {
+#       "not": {
+#         "field": "location",
+#         "in": "[parameters('allowedLocations')]"
+#       }
+#     },
+#     "then": {
+#       "effect": "audit"
+#     }
+#   }
+# POLICY_RULE
 
-  parameters = <<PARAMETERS
-    {
-    "allowedLocations": {
-      "type": "Array",
-      "metadata": {
-        "description": "The list of allowed locations for resources.",
-        "displayName": "Allowed locations",
-        "strongType": "location"
-      }
-    }
-  }
-PARAMETERS
-}
+#   parameters = <<PARAMETERS
+#     {
+#     "allowedLocations": {
+#       "type": "Array",
+#       "metadata": {
+#         "description": "The list of allowed locations for resources.",
+#         "displayName": "Allowed locations",
+#         "strongType": "location"
+#       }
+#     }
+#   }
+# PARAMETERS
+# }
 
 
