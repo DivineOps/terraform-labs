07a043bd50bc1e663d6549f329aca97f74268534
Joey Lorich <joseph@Lorich.me>
2019-07-25 12:17:51 -0600

    change sp to vars

--- aks_advnet_rbac/main.tf
+++ aks_advnet_rbac/main.tf
@@ -128,8 +128,8 @@ resource "azurerm_kubernetes_cluster" "test" {
   }
 
   service_principal {
-    client_id     = "${env.ARM_CLIENT_ID}"
-    client_secret = "${env.ARM_CLIENT_SECRET}"
+    client_id     = "${var.ARM_CLIENT_ID}"
+    client_secret = "${var.ARM_CLIENT_SECRET}"
   }
 
   role_based_access_control {
